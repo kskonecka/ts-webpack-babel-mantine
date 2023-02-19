ef55deb8b6aa92d585639de7632c8688235685e0
Kasia Skonecka <katarzyna.skonecka@lego.com>
2023-02-19 15:51:10 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Hero} from "./Hero";
+
+export const App: React.FC = () => {
+    return <Hero/>
+}
---
+++ src/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mantine/core";
+
+export const Hero = () => {
+  return (
+    <Container size={"md"}>
+      <h1>HERO</h1>
+    </Container>
+  );
+};
