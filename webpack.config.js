1b91cab6c1cf9d6584d34ce299531de5ab390084
Kasia Skonecka <katarzyna.skonecka@lego.com>
2023-02-20 19:58:53 +0100

    add output directory build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/Index.tsx',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: "bundle.js"
+  entry: "./src/Index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./build"),
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, './dist')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.jsx', '.js', '.ts', '.tsx']
-    },
-    // performance: {
-    //     hints: false,
-    // },
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".ts", ".tsx"],
+  },
+  // performance: {
+  //     hints: false,
+  // },
+};
